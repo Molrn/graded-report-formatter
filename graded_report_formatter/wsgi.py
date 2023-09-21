be9e6f139d071e0f9537745ef0b5731e2ccafc61
Molrn <youri.aubry@utbm.fr>
2023-09-21 19:42:01 +0200

    Add empty Django project

---
+++ graded_report_formatter/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for graded_report_formatter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graded_report_formatter.settings')
+
+application = get_wsgi_application()
