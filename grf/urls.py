63aaa165a265bbd87ac7baec99883b572872b964
Molrn <youri.aubry@utbm.fr>
2023-09-27 18:58:15 +0200

    Feature: Add templates

--- grf/urls.py
+++ grf/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("report/<int:id>", views.ReportView.as_view(), name="report"),
+    path("template/<int:id>", views.TemplateView.as_view(), name="template"),
     path("report/create", views.ReportCreateView.as_view(), name="report-create"),
     path("settings", views.SettingsView.as_view(), name="settings"),
 ]
--- grf/views.py
+++ grf/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Report
+from .models import Report, Template
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
@@ -8,6 +8,7 @@ class IndexView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['all_reports'] = Report.objects.all()
+        context['all_templates'] = Template.objects.all()
         return context
 
 class ReportView(generic.DetailView):
@@ -21,6 +22,14 @@ class ReportView(generic.DetailView):
 class ReportCreateView(generic.CreateView):
     model = Report
     template_name = 'report/create.html'
+
+class TemplateView(generic.DetailView):
+    model = Template
+    template_name = 'template/edit.html'
+    
+    def get_object(self, queryset=None):
+        id = self.kwargs.get("id")
+        return Template.objects.get(pk=id)
 
 class SettingsView(generic.UpdateView):
     template_name = 'settings.html'
