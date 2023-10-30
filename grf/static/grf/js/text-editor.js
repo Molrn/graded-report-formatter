52180322edb8305166b9fdf4556214c1baaa502f
Molrn <youri.aubry@utbm.fr>
2023-10-30 23:15:26 +0100

    Fix: Text editor default html plain to formatted

--- grf/static/grf/js/text-editor.js
+++ grf/static/grf/js/text-editor.js
@@ -1,32 +1,39 @@
 let textEditorsElements = document.getElementsByClassName( 'text-editor' );
 for (let index = 0; index < textEditorsElements.length; index++) {
-    ClassicEditor.create(textEditorsElements[index], {
-        toolbar: {
-            items: [
-                'bold',
-                'italic',
-                '|',
-                'bulletedList',
-                'numberedList',
-                '|',
-                'link',
-                'imageUpload',
-                '|',
-                'undo',
-                'redo'
-            ],
-            shouldNotGroupWhenFull: true,
-            removeButtons: ''
-        },
-        language: 'en',
-        image: {
-            toolbar: [
-                'imageTextAlternative',
-                'imageStyle:full',
-                'imageStyle:side'
-            ]
-        } 
-    });
+    ClassicEditor
+        .create(textEditorsElements[index], {
+            toolbar: {
+                items: [
+                    'bold',
+                    'italic',
+                    '|',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'link',
+                    'imageUpload',
+                    '|',
+                    'undo',
+                    'redo'
+                ],
+                shouldNotGroupWhenFull: true,
+                removeButtons: ''
+            },
+            language: 'en',
+            image: {
+                toolbar: [
+                    'imageTextAlternative',
+                    'imageStyle:full',
+                    'imageStyle:side'
+                ]
+            } 
+        })
+        .then( editor => {
+            editor.setData(textEditorsElements[index].getAttribute('html-content'));
+        } )
+        .catch( error => {
+            console.error( 'There was a problem initializing the editor.', error );
+        } );
 }
 
 
