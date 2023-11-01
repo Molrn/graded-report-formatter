5752f3b52bf3e56894feb325eaa94e89e1951a8b
Molrn <youri.aubry@utbm.fr>
2023-11-01 16:15:33 +0100

    Format : gather report event listeners into one file

---
+++ grf/static/grf/js/report-event-listeners.js
@@ -0,0 +1,106 @@
+const initFunctions = [setSortableAccordion, setGradeSelect, setGradeToggle, setAddingButtons, setDeleteReportElementButtons];
+
+for (let i = 0; i < initFunctions.length; i++) {
+    document.addEventListener('DOMContentLoaded', initFunctions[i]);
+}
+
+function setGradeSelect(){
+    var gradeSelect = document.getElementsByClassName('grade-select');
+    const gradesArray = [1, 2, 3, 4, 5];
+    for (let index = 0; index < textEditorsElements.length; index++) {
+        if(gradeSelect[index]){
+            let value = gradeSelect[index].getAttribute('value');
+            if(!gradesArray.includes(value)){
+                gradeSelect[index].value='empty';
+            } else {
+                gradeSelect[index].value=value;
+            }
+        }
+    }
+}
+
+function setAddingButtons(){
+    const templatePart = document.querySelector('#template-part > .accordion-item');
+    const templateSubPart = document.querySelector('#template-part > .accordion-item');
+    const templateDirectSubPart = document.querySelector('#template-direct-subpart > .accordion-item');
+    const addingButtons = document.getElementsByClassName('add-accordion-item');
+
+    for (let i = 0; i < addingButtons.length; i++) {
+        addingButtons[i].addEventListener('click', function (event) {
+            const accordionContent = this.parentNode;
+            if (this.classList.contains('add-subpart')) { 
+                accordionContent.insertBefore(templateSubPart.cloneNode(true), event.target);
+            } else if (this.classList.contains('add-part')){
+                accordionContent.insertBefore(templatePart.cloneNode(true), event.target);
+            }else if (this.classList.contains('add-direct-subpart')){
+                accordionContent.insertBefore(templateDirectSubPart.cloneNode(true), event.target);
+            }
+            setSortableAccordion();
+            setAddingButtons();
+            setDeleteReportElementButtons();
+        });
+    }
+}
+
+function setGradeToggle(){
+    const gradeToggles = document.getElementsByClassName('grade-toggle');
+    for (let i = 0; i < gradeToggles.length; i++) {
+        gradeToggles[i].addEventListener('change', function () {
+            const accordionItem = this.parentElement.parentElement;
+            const headerPath = '.accordion-content > .accordion > .accordion-item > .accordion-header';
+            const gradeSelects = accordionItem.querySelectorAll(headerPath + ' > .grade-select');
+            const gradeLabels = accordionItem.querySelectorAll(headerPath + ' > .grade-label');
+            if (this.checked) {
+                gradeSelects.forEach(function (select) {select.style.display = 'block';});
+                gradeLabels.forEach(function (select) {select.style.display = 'block';});
+            } else {
+                gradeSelects.forEach(function (select) {select.style.display = 'none';});
+                gradeLabels.forEach(function (select) {select.style.display = 'none';});
+            }
+        });
+    }
+}
+
+function setSortableAccordion(event){
+    $('.accordion-toggle').click(function () {
+        $(this).parent('.accordion-item').toggleClass('active');
+    });
+
+    // Make accordion items sortable within their parent accordion
+    $('.accordion').each(function () {
+        $(this).sortable({
+            items: '.accordion-item',
+            handle: '.accordion-drag-handle',
+            tolerance: 'pointer',
+            connectWith: '.accordion#' + $(this).attr('id'),
+        });
+    });
+
+    // Prevent dragging on accordion content
+    $('.accordion-content').on('mousedown', function (e) {
+        e.stopPropagation();
+    });
+
+    // Make the accordion collapsible
+    $('.accordion .accordion-item').each(function () {
+        var $header = $(this).find('.accordion-header');
+        var $content = $(this).find('.accordion-content');
+        var $toggleButton = $header.find('.accordion-toggle');
+
+        if (!$(this).hasClass('active')) {
+            $toggleButton.html('▼'); 
+        } else {
+            $toggleButton.html('▲'); 
+        }
+
+        $toggleButton.click(function (event) {
+            event.stopImmediatePropagation();
+            $(this).parent('.accordion-header').next('.accordion-content').slideToggle();
+            if ($(this).html() == '▼') {
+                $(this).html('▲'); 
+            } else {
+                $(this).html('▼'); 
+            }
+        });
+    });
+}
