d5e1466414d2aa400e734b8af1c635351455e355
Molrn <76754000+Molrn@users.noreply.github.com>
2023-12-14 13:18:52 +0100

    Refactor: gather JS CRUD operations to functions

---
+++ grf/static/grf/js/report-CRUD.js
@@ -0,0 +1,114 @@
+function openReportCreationModal(){
+    document.getElementById('modal-container').style.display = 'block';
+}
+
+function closeReportCreationModal(){
+    document.getElementById('modal-container').style.display = 'none';
+}
+
+async function createReport(){
+    const newReportTitle = document.getElementById('new-report-title').value;
+    const templateReportId = parseInt(document.getElementById('template-report').value);
+    var newReport;
+    if (templateReportId){
+        newReport = await getReportDict(templateReportId, true);
+        newReport.title = newReportTitle; 
+    } else {
+        newReport = { 
+            id: null,
+            parts: [],
+            title: newReportTitle
+        };
+    }
+    const reportId = await sendReport(newReport);    
+    closeReportCreationModal();
+    window.location.href = 'report/'+reportId.toString();
+
+}
+
+function deleteReport(button) {
+    const reportId = button.getAttribute('data-report-id');
+    fetch(`/grf/delete_report/${reportId}`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        location.reload();
+    })
+    .catch(error => { console.error(error); });
+}
+
+const fileSelector = document.getElementById('import-file-selector');
+const uploadButton = document.getElementById('json-import-button');
+
+uploadButton.addEventListener('click', function () {
+    fileSelector.click();
+});
+
+fileSelector.addEventListener('change', function () {
+    if (fileSelector.files.length > 0) {
+        const file = fileSelector.files[0];
+        const reader = new FileReader();
+        reader.onload = function () {
+            try {
+                dictReport = JSON.parse(reader.result);
+                sendReport(dictReport);
+                setTimeout(function() {
+                    location.reload();
+                }, 1000);
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                alert('Invalid JSON file. Please select a valid JSON file.');
+            }
+        };
+        reader.readAsText(file);
+    }
+});
+
+function saveReport(){
+    const dictReport = reportToDict();
+    sendReport(dictReport);
+}
+
+function exportToJSON(){
+    const dictReport = reportToDict(true);
+    const jsonReport = JSON.stringify(dictReport);
+    const blob = new Blob([jsonReport], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = dictReport['title']+".json";
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+async function exportToPDF() {
+    const reportTitle = document.getElementById('report-title').value;
+    const dictReport = reportToDict();
+    const reportId = await sendReport(dictReport, skip_alert = true);
+
+    fetch('/grf/report/' + reportId + '/display')
+        .then(response => response.text())
+        .then(html => {
+            var element = document.getElementById('editable-style');
+            element.style.display = 'none';
+
+            var opt = {
+                margin: 10,
+                filename: reportTitle + '.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+            };
+
+            html2pdf().from(html).set(opt).outputPdf().then(pdf => {
+                pdf.save();
+                element.style.display = 'block';
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
