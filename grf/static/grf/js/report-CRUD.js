5967f5e71b9c0a5b3a112e6a3ecb1ad60f62f7d8
Molrn <76754000+Molrn@users.noreply.github.com>
2023-12-14 13:37:46 +0100

    Fix: convert import event listeners to functions

--- grf/static/grf/js/report-CRUD.js
+++ grf/static/grf/js/report-CRUD.js
@@ -40,21 +40,14 @@ function deleteReport(button) {
     .catch(error => { console.error(error); });
 }
 
-const fileSelector = document.getElementById('import-file-selector');
-const uploadButton = document.getElementById('json-import-button');
-
-uploadButton.addEventListener('click', function () {
-    fileSelector.click();
-});
-
-fileSelector.addEventListener('change', function () {
+async function importReport(){
     if (fileSelector.files.length > 0) {
         const file = fileSelector.files[0];
         const reader = new FileReader();
-        reader.onload = function () {
+        reader.onload = async function () {
             try {
                 dictReport = JSON.parse(reader.result);
-                sendReport(dictReport);
+                await sendReport(dictReport);
                 setTimeout(function() {
                     location.reload();
                 }, 1000);
@@ -65,7 +58,7 @@ fileSelector.addEventListener('change', function () {
         };
         reader.readAsText(file);
     }
-});
+}
 
 function saveReport(){
     const dictReport = reportToDict();
